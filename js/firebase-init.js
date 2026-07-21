4fd0bbd70f2bb9760376e805ffd96559b24c488d
Nearchos <129533209+nenikolaidis@users.noreply.github.com>
2026-07-22 02:23:04 +0300

    Remove Firebase initialization code

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -4,16 +4,3 @@
    (The compat SDK scripts are loaded in index.html before this module,
    so the global `firebase` object is already available here.)
 ========================================================= */
-const firebaseConfig = {
-  apiKey: "AIzaSyD-xJIVx-E_GyBkFMJwLdNvSIYHYT_7Rt8",
-  authDomain: "devflow-board-11146.firebaseapp.com",
-  projectId: "devflow-board-11146",
-  storageBucket: "devflow-board-11146.firebasestorage.app",
-  messagingSenderId: "725523676463",
-  appId: "1:725523676463:web:2574564826cee68220f5da"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
