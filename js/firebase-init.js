0d482a5733ff3008fa71688a01ddf1c7a62eab9b
Nearchos <129533209+nenikolaidis@users.noreply.github.com>
2026-07-22 02:03:41 +0300

    Update Firebase configuration for new project

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -5,12 +5,12 @@
    so the global `firebase` object is already available here.)
 ========================================================= */
 const firebaseConfig = {
-  apiKey: "AIzaSyDU6gphWwsBhGBx5XXmiBg07frrBkHriZM",
-  authDomain: "devflow-board-b45c1.firebaseapp.com",
-  projectId: "devflow-board-b45c1",
-  storageBucket: "devflow-board-b45c1.firebasestorage.app",
-  messagingSenderId: "80877882513",
-  appId: "1:80877882513:web:521270b5460359c8c6c330"
+  apiKey: "AIzaSyD-xJIVx-E_GyBkFMJwLdNvSIYHYT_7Rt8",
+  authDomain: "devflow-board-11146.firebaseapp.com",
+  projectId: "devflow-board-11146",
+  storageBucket: "devflow-board-11146.firebasestorage.app",
+  messagingSenderId: "725523676463",
+  appId: "1:725523676463:web:2574564826cee68220f5da"
 };
 
 firebase.initializeApp(firebaseConfig);
